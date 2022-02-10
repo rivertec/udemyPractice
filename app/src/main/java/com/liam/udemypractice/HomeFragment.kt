1122da329244117865a97809acabec33aba1a6c1
rivertec <oriver1129@gmail.com>
2022-02-09 19:49:14 -0500

    Project 7, Add json file and class to read it

--- app/src/main/java/com/liam/udemypractice/HomeFragment.kt
+++ app/src/main/java/com/liam/udemypractice/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.json.JSONObject
 
 class HomeFragment : Fragment() {
 
@@ -29,5 +30,20 @@ class HomeFragment : Fragment() {
 
         val assetLoader = AssetLoader()
         val homeData = assetLoader.getJasonString(requireContext(), "home.json")
+
+        if (!homeData.isNullOrEmpty()) {
+            val jsonObject = JSONObject(homeData)
+            val title = jsonObject.getJSONObject("title")
+            val text = title.getString("text")
+            val iconUrl = title.getString("icon_url")
+
+            val topBanners = jsonObject.getJSONArray("top_banners")
+            val firstBanner = topBanners.getJSONObject(0)
+            val label = firstBanner.getString("label")
+            val productDetail = firstBanner.getJSONObject("product_detail")
+            val price = productDetail.getInt("price")
+
+            Log.d("title","text=${text}, iconUrl = ${iconUrl})")
+        }
     }
 }
---
+++ app/src/main/java/com/liam/udemypractice/Title.kt
@@ -0,0 +1,6 @@
+package com.liam.udemypractice
+
+data class Title(
+    val text: String,
+    val iconUrl: String
+)
