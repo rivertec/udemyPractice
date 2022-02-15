75e85705f7c79e2d2f3f227179f1b13d263ae271
rivertec <oriver1129@gmail.com>
2022-02-15 00:41:20 -0500

    Messed up

--- app/src/main/java/com/liam/udemypractice/ui/home/HomeBannerAdapter.kt
+++ app/src/main/java/com/liam/udemypractice/ui/home/HomeBannerAdapter.kt
@@ -5,8 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.liam.udemypractice.Banner
 import com.liam.udemypractice.databinding.ItemHomeBannerBinding
+import com.liam.udemypractice.model.Banner
 
 class HomeBannerAdapter :
     ListAdapter<Banner, HomeBannerAdapter.HomeBannerViewHolder>(
